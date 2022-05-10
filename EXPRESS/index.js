f1c2c266996ccb954f0791c88a645ff47ce1f478
David Pires WorkPlace <developer.david@outlook.com>
2022-05-10 17:10:01 -0300

    feat: route

--- EXPRESS/index.js
+++ EXPRESS/index.js
@@ -1,6 +1,13 @@
 const express = require("express"); // importando o express
 const app = express(); // iniciando o express e repassando para a variavel app
 
+//.post
+//.delete
+//varias formas de criar rotas 
+app.get("/",function(req, resp) {
+  resp.send('Bem vindo ao Guia de Dev')
+})
+
 app.listen(4000,function(erro) {
   if (erro) {
     console.log("ocorreu um erro!");
