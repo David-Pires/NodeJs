f6246a0d89d7c9bc7292686c92aa23b9db73c412
David Pires WorkPlace <developer.david@outlook.com>
2022-05-10 17:15:39 -0300

    feat: Creating more routes

--- EXPRESS/index.js
+++ EXPRESS/index.js
@@ -4,15 +4,23 @@ const app = express(); // iniciando o express e repassando para a variavel app
 //.post
 //.delete
 //varias formas de criar rotas 
-app.get("/",function(req, resp) {
-  resp.send('Bem vindo ao Guia de Dev')
+app.get("/",function(req, res) {
+  res.send('Bem vindo ao Guia de Dev')
+})
+
+app.get("/blog", function(req, res) {
+  res.send("Bem vindo ao meu blog")
+})
+
+app.get("/canal/youtube", function(req, res) {
+  res.send("<h1>Bem vindo a minha rota</h1>")
 })
 
 app.listen(4000,function(erro) {
   if (erro) {
-    console.log("ocorreu um erro!");
+    console.log("ocorreu um erro!")
   } else {
-    console.log('Servidor iniciado com sucesso');
+    console.log('Servidor iniciado com sucesso')
   }
 })
 
